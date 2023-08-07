29dde753c348e41d806de048a86be525656225bc
Danail Dimov <92359222+danaildimovv@users.noreply.github.com>
2023-08-07 09:20:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -491,6 +491,20 @@ function checkWindowSize() {
    else{
       addFiltersBehaviour();
    }
+   if (window.innerWidth < 770){
+      
+      let previousScrollPosition = window.scrollY;
+      window.onscroll = function() {
+         let currentScrollPosition = window.scrollY;
+         if (previousScrollPosition > currentScrollPosition) {
+          document.querySelector(".products-info").style.top = "12vh";
+         } 
+         else {
+            document.querySelector(".products-info").style.top = "-50vh";
+         }
+         previousScrollPosition = currentScrollPosition;
+      }
+   }
  }
  
 window.onresize = checkWindowSize;
@@ -525,18 +539,4 @@ hamburgerFilter.onclick = function(){
 let closeFilterButton = document.querySelector("#button-close");
 closeFilterButton.onclick = function(){
   sidebar.classList.toggle("active");
-}
-
-let previousScrollPosition = window.scrollY;
-window.onscroll = function() {
-let currentScrollPosition = window.scrollY;
-   
-  if (previousScrollPosition > currentScrollPosition) {
-    document.querySelector(".products-info").style.top = "12vh";
-  } 
-  else {
-    document.querySelector(".products-info").style.top = "-50vh";
-  }
-   
-  previousScrollPosition = currentScrollPosition;
 }
